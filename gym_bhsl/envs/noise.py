c1036bf5815c4532a460f67c6b1c381dc2dece6f
tjkemp <tero.kemppi@gmail.com>
2021-06-06 18:15:18 +0300

    Use float instead of np.float in internal state

--- gym_bhsl/envs/noise.py
+++ gym_bhsl/envs/noise.py
@@ -4,7 +4,7 @@ import numpy as np
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, mu=0.0, theta=0.15, sigma=0.2):
+    def __init__(self, mu: float = 0.0, theta: float = 0.15, sigma: float = 0.2):
         """Initialize the process.
 
         Args:
@@ -13,22 +13,25 @@ class OUNoise:
             sigma: the "amplitude", values will be roughly [-6*sigma, 6*sigma].
 
         """
-        self.mu = mu * np.ones(1)
+        self.mu = mu
         self.theta = theta
         self.sigma = sigma
         self.reset()
 
-    def seed(self, seed) -> None:
-        """Set seed for reproducible randomness."""
+    def seed(self, seed: int) -> None:
+        """Set seed for reproducible randomness.
+
+        Note that `numpy.random.seed` is not thread-safe.
+        """
         np.random.seed(seed)
 
     def reset(self) -> None:
         """Reset the internal state to mean (mu)."""
-        self.state = self.mu.copy()
+        self.state = self.mu
 
     def sample(self) -> float:
         """Update internal state and return it as a sample."""
         x = self.state
         dx = self.theta * (self.mu - x) + self.sigma * np.random.randn()
         self.state = x + dx
-        return self.state[0]
+        return self.state
