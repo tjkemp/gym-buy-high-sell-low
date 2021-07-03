96447109156da434910d78bfab16f2cf164cf7ab
tjkemp <tero.kemppi@gmail.com>
2021-07-03 11:51:40 +0300

    Add helpers for calculating moving averages

---
+++ gym_bhsl/math/average.py
@@ -0,0 +1,33 @@
+import itertools
+from typing import List, Union
+
+import numpy as np
+
+
+def moving_average(values: Union[List[float], List[int]], window: int) -> List[float]:
+    """Calculate moving average.
+
+    Args:
+        values: a list of values for which to calculate average
+        window: the length of window for which to calculate moving averages
+
+    Returns:
+        a list of floats of the size `values - windows + 1`
+
+    Example:
+        >>> moving_average([0, 0, 1, 2, 4, 5, 4], 3)
+        [0.33333, 1.0, 2.33333, 3.66666, 4.33333]
+
+    """
+    if window > len(values):
+        raise ValueError("Window length must be less or equal to length of values")
+    average = np.convolve(values, np.ones(window), "valid") / window
+    return average.tolist()
+
+
+def right_average(values: list, num: int) -> float:
+    """Calculate average of `num` rightmost values in a list."""
+    if not 0 < num <= len(values):
+        raise ValueError(f"Invalid value for average: {num}")
+    # return sum(values[len(values)-num:]) / num
+    return sum(itertools.islice(values, len(values) - num, len(values))) / num
