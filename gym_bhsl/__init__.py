23755a9bae3b9a488d3457096739e34223468534
tjkemp <tero.kemppi@gmail.com>
2021-08-05 16:19:03 +0300

    Change gym identifier into BuyHighSellLow-v0

--- gym_bhsl/__init__.py
+++ gym_bhsl/__init__.py
@@ -3,6 +3,6 @@ from gym.envs.registration import register
 __VERSION__ = "0.1.1"
 
 register(
-    id="buy-high-sell-low-v0",
+    id="BuyHighSellLow-v0",
     entry_point="gym_bhsl.envs:BuyHighSellLow",
 )
